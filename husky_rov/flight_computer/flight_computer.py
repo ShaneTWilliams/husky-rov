17117df614824f5d89883750d8b48c47b7a59d05
ShaneTWilliams <shanethomaswilliams@gmail.com>
2019-01-17 14:51:43 -0330

    Change Flight Computer IP address

--- husky_rov/flight_computer/flight_computer.py
+++ husky_rov/flight_computer/flight_computer.py
@@ -3,4 +3,4 @@ from rov_server import ROVServer
 if __name__ == '__main__':
     port = int(input('Enter a port for the ROV server: '))
     print('Server running on 192.168.2.99 at port {}'.format(port))
-    ROVServer('192.168.2.99', port)
+    ROVServer('192.168.0.98', port)
