4f99927cbada46b8d934ab7175ce4a9dd62ac177
ShaneTWilliams <shanethomaswilliams@gmail.com>
2019-03-03 19:35:12 -0330

    a

--- pilot_terminal/terminal.py
+++ pilot_terminal/terminal.py
@@ -183,6 +183,7 @@ class PilotTerminal(QtWidgets.QMainWindow):
 
     def keyPressEvent(self, event):
         key = event.key()
+        print(key)
         if not event.isAutoRepeat():
             if key == QtCore.Qt.Key_Enter:
                 self.show_video_dialog()
